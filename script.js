9bffcc658204add90149006f407e46981f2ebf3f
BlairCalgary <59458311+BlairCalgary@users.noreply.github.com>
2020-02-07 16:15:27 -0700

    add onlick to cards

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ vegans.forEach((result, id) => {
     result.image === undefined || result.image === "" ? result.image = "img/vegan-logo.png" : result.image; 
     //build the card content
     const content = `
-        <div class='card'>
+        <div class='card' onclick="window.open('http://${result.contact}')" target="_blank">
             <h3>${result.name}</h3>
             <img class="cardImg push" src=${result.image}>
             <p class="push">${result.contact}<p>
