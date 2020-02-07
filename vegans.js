f411ddd2a9fd907b3b78b8df6aae527889e6679a
Fletchlock <47433327+Fletchlock@users.noreply.github.com>
2020-02-07 10:50:36 -0700

    changed index added new script styles and images

---
+++ vegans.js
@@ -0,0 +1,122 @@
+const vegans = [
+    {
+        name: "Jon Snow",
+        contact: "@JonSnow",
+        image: "img/jonsnow.png"
+    },
+    {
+        name: "Darth Vader",
+        contact: "@ImperialLeader",
+        image: "img/darthvader.png"
+    },
+    {
+        name: "Donald Duck",
+        contact: "www.DonaldDuck.com",
+        image: "img/donaldduck.png"
+    },
+    {
+        name: "Slash",
+        contact: "@SlashGNR",
+        image: "img/slash.png"
+    },
+    {
+        name: "Jon Snow",
+        contact: "@JonSnow"
+    },
+    {
+        name: "Darth Vader",
+        contact: "@ImperialLeader"
+    },
+    {
+        name: "Donald Duck",
+        contact: "www.DonaldDuck.com"
+    },
+    {
+        name: "Slash",
+        contact: "@SlashGNR",
+        image: "img/slash.png"
+    },
+    {
+        name: "Jon Snow",
+        contact: "@JonSnow"
+    },
+    {
+        name: "Darth Vader",
+        contact: "@ImperialLeader",
+        image: ""
+    },
+    {
+        name: "Donald Duck",
+        contact: "www.DonaldDuck.com"
+    },
+    {
+        name: "Slash",
+        contact: "@SlashGNR"
+    },        
+    {
+        name: "Jon Snow",
+        contact: "@JonSnow"
+    },
+    {
+        name: "Darth Vader",
+        contact: "@ImperialLeader"
+    },
+    {
+        name: "Donald Duck",
+        contact: "www.DonaldDuck.com"
+    },
+    {
+        name: "Slash",
+        contact: "@SlashGNR"
+    },
+    {
+        name: "Jon Snow",
+        contact: "@JonSnow"
+    },
+    {
+        name: "Darth Vader",
+        contact: "@ImperialLeader"
+    },
+    {
+        name: "Donald Duck",
+        contact: "www.DonaldDuck.com"
+    },
+    {
+        name: "Slash",
+        contact: "@SlashGNR"
+    },
+    {
+        name: "Jon Snow",
+        contact: "@JonSnow"
+    },
+    {
+        name: "Darth Vader",
+        contact: "@ImperialLeader"
+    },
+    {
+        name: "Donald Duck",
+        contact: "www.DonaldDuck.com"
+    },
+    {
+        name: "Slash",
+        contact: "@SlashGNR"
+    },
+    {
+        name: "Jon Snow",
+        contact: "@JonSnow"
+    },
+    {
+        name: "Darth Vader",
+        contact: "@ImperialLeader"
+    },
+    {
+        name: "Donald Duck",
+        contact: "www.DonaldDuck.com"
+    },
+    {
+        name: "Slash",
+        contact: "@SlashGNR"
+    }            
+];
+
+export default vegans;
